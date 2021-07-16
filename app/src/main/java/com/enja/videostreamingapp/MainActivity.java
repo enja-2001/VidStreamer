74b75a49e41dfffe298bd894554fda019def26a4
enja-2001 <munmunneshasweet@gmail.com>
2021-07-16 18:23:47 +0530

    Exoplayer added

--- app/src/main/java/com/enja/videostreamingapp/MainActivity.java
+++ app/src/main/java/com/enja/videostreamingapp/MainActivity.java
@@ -2,13 +2,103 @@ package com.enja.videostreamingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.Player;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.ProgressiveMediaSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 public class MainActivity extends AppCompatActivity {
+
+    PlayerView playerView;
+    ProgressBar progressBar;
+    SimpleExoPlayer simpleExoPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        init();
+        initializePlayer();
+    }
+
+    private void init(){
+        playerView=findViewById(R.id.playerView);
+        progressBar=findViewById(R.id.progressBar);
+    }
+
+    private void initializePlayer() {
+        Uri videoUri = Uri.parse("http://ak-ind-cdn.snackvideo.in/upic/2021/03/09/14/BMjAyMTAzMDkxNDEzMzhfMTUwMDAwNTA0NzAxODE3XzE1MDAwMTIzNjc5MTk0Nl8wXzM=_b_B39ea1dc9a84778c3301cdd9fdb62fc80.mp4?tag=1-1618597049-s-0-t8ie3aggzf-797c984891af85f7");
+
+        simpleExoPlayer = new SimpleExoPlayer.Builder(MainActivity.this).build();
+
+        simpleExoPlayer.addListener(new Player.EventListener(){
+            @Override
+            public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                if(playbackState == Player.STATE_BUFFERING){
+                    progressBar.setVisibility(View.VISIBLE);
+                }
+                else if(playbackState == Player.STATE_READY){
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+        String userAgent = Util.getUserAgent(playerView.getContext(),playerView.getContext().getString(R.string.app_name));
+        MediaSource mediaSource = new ProgressiveMediaSource.Factory(new DefaultHttpDataSourceFactory(userAgent),
+                new DefaultExtractorsFactory()).createMediaSource(videoUri);
+
+        playerView.setPlayer(simpleExoPlayer);
+        playerView.setKeepScreenOn(true);
+        simpleExoPlayer.prepare(mediaSource);
+        simpleExoPlayer.getPlayWhenReady();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if(hasFocus)
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        simpleExoPlayer.setPlayWhenReady(false);
+        simpleExoPlayer.getPlaybackState();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        simpleExoPlayer.setPlayWhenReady(true);
+        simpleExoPlayer.getPlaybackState();
     }
 }
